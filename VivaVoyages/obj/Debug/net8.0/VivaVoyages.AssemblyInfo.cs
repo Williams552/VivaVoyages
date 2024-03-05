19b97a81a007e120cd5e85a7156aaa546dd04978
AugustP <phongbigguy23@gmail.com>
2024-03-06 02:40:27 +0700

    TourManagement, Update gender profile

--- VivaVoyages/obj/Debug/net8.0/VivaVoyages.AssemblyInfo.cs
+++ VivaVoyages/obj/Debug/net8.0/VivaVoyages.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VivaVoyages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8673daa746773aaa9f9f09ccf3dd7e5debdeadab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14baf0ffd43fd3380de06c7fa84d1387ab73c16e")]
 [assembly: System.Reflection.AssemblyProductAttribute("VivaVoyages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VivaVoyages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
