04d470eb62fc9d383545bb8a20cbc356ad93d821
TruongLN265 <Truonglee2605@gmail.com>
2024-03-11 00:15:45 +0700

    fix loi search, phan trang list tour

--- VivaVoyages/obj/Debug/net8.0/VivaVoyages.AssemblyInfo.cs
+++ VivaVoyages/obj/Debug/net8.0/VivaVoyages.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VivaVoyages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3de1f320f9f0977bfe8113b42a07d073ee003cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3bcf13ad3270efb517603d045505587786846db")]
 [assembly: System.Reflection.AssemblyProductAttribute("VivaVoyages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VivaVoyages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
