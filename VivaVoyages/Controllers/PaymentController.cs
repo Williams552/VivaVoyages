a974387ace97ff85b6f2a6644533ec6670c7d374
AugustP <phongbigguy23@gmail.com>
2024-03-18 14:34:27 +0700

    View Payment

---
+++ VivaVoyages/Controllers/PaymentController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace VivaVoyages.Controllers
+{
+    
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            // TODO: Your code here
+            return View();
+        }
+        
+    }
+}
