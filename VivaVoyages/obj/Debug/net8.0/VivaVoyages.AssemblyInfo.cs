004230642a1d30b7674ec8aaa8ea338b90ef6efc
AugustP <phongbigguy23@gmail.com>
2024-03-20 21:39:06 +0700

    Update Payment

--- VivaVoyages/obj/Debug/net8.0/VivaVoyages.AssemblyInfo.cs
+++ VivaVoyages/obj/Debug/net8.0/VivaVoyages.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VivaVoyages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d70253fa0f67d39bab2292c87b6e24d8b474bf2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec02e362958532b6ff1042e0a027533803924106")]
 [assembly: System.Reflection.AssemblyProductAttribute("VivaVoyages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VivaVoyages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
