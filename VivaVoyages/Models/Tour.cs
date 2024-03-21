201d076444c0d3372331dfc5bb71f74db3decb4a
Williams552 <tiendat552@gmail.com>
2024-03-22 00:03:00 +0700

    Khá là ok rồi

--- VivaVoyages/Models/Tour.cs
+++ VivaVoyages/Models/Tour.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace VivaVoyages.Models;
 
@@ -7,22 +8,29 @@ public partial class Tour
 {
     public int TourId { get; set; }
 
+    [DisplayName("Tour Name")]
     public string TourName { get; set; } = null!;
 
+    [DisplayName("Expected Profit")]
     public decimal ExpectedProfit { get; set; }
 
+    [DisplayName("Date Start")]
     public DateOnly DateStart { get; set; }
 
+    [DisplayName("Tour Dates")]
     public int TourDates { get; set; }
 
+    [DisplayName("Max Passeger")]
     public int MaxPasseger { get; set; }
 
+    [DisplayName("Tour Guide")]
     public string TourGuide { get; set; } = null!;
 
     public decimal Cost { get; set; }
 
     public decimal? Tax { get; set; }
 
+    [DisplayName("Single Room Cost")]
     public decimal? SingleRoomCost { get; set; }
 
     public string? ImagePath { get; set; }
--- VivaVoyages/obj/Debug/net8.0/VivaVoyages.AssemblyInfo.cs
+++ VivaVoyages/obj/Debug/net8.0/VivaVoyages.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VivaVoyages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d9133827d7e325437c6b22d5ebbce4391b63b03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3f252bf86e8ea546321b147612c322e3b3b358c")]
 [assembly: System.Reflection.AssemblyProductAttribute("VivaVoyages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VivaVoyages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
