5387d1939fed080a1e6a9871630b6d362e38e9da
carturbo69 <carturbo69@gmail.com>
2024-03-21 14:32:53 +0700

    1 chut

--- VivaVoyages/obj/Debug/net8.0/VivaVoyages.AssemblyInfo.cs
+++ VivaVoyages/obj/Debug/net8.0/VivaVoyages.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VivaVoyages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c667bb7e74a59dbc9d3594a773b6d7e115ec62c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+477115c3e10e8e5c24b863f36090b5d4597db0b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("VivaVoyages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VivaVoyages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
