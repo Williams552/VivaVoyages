c1d2f3c8a9bc52810b52965191a65b89f2eb6d03
Williams552 <tiendat552@gmail.com>
2024-03-21 21:55:15 +0700

    Bắt một số lỗi, Passenger, DoB...

--- VivaVoyages/obj/Debug/net8.0/VivaVoyages.AssemblyInfo.cs
+++ VivaVoyages/obj/Debug/net8.0/VivaVoyages.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VivaVoyages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1be809582da5648905e3211b1d91c938ba49423a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42b0e00a9d89fa1c6922162ad866143fb7ec39ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("VivaVoyages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VivaVoyages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
