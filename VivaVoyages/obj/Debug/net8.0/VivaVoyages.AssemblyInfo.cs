e81392e25257f42580e30a8860e02cb16d43feea
carturbo69 <carturbo69@gmail.com>
2024-03-21 20:25:05 +0700

    PREvent

--- VivaVoyages/obj/Debug/net8.0/VivaVoyages.AssemblyInfo.cs
+++ VivaVoyages/obj/Debug/net8.0/VivaVoyages.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VivaVoyages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+685aba9dc6e1a712c9ec36575b25474f38ba9a81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4c9c9299af0f3c7daa5cd650bc2928f72506b47")]
 [assembly: System.Reflection.AssemblyProductAttribute("VivaVoyages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VivaVoyages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
