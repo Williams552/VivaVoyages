06d5c48a12a5b848aad99315398c8170ee6f51da
TruongLN265 <Truonglee2605@gmail.com>
2024-03-21 22:08:48 +0700

    Commit lan cuoi cung

--- VivaVoyages/obj/Debug/net8.0/VivaVoyages.AssemblyInfo.cs
+++ VivaVoyages/obj/Debug/net8.0/VivaVoyages.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VivaVoyages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c07d72e35d4494f726c2f1d96794e30877d9989")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1be809582da5648905e3211b1d91c938ba49423a")]
 [assembly: System.Reflection.AssemblyProductAttribute("VivaVoyages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VivaVoyages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
